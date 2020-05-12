0ba67a59f62ede3ed3af25488cbdeceed3d4a39a
xiaoluarray <1561754067@qq.com>
2020-05-12 21:21:17 +0800

    git test1

---
+++ src/reflection/GitTest.java
@@ -0,0 +1,15 @@
+package reflection;
+
+public class GitTest {
+    private static int s = 8;
+    public static void main(String[] args) {
+        System.out.println("git test");
+
+    }
+    public static int test(){
+        return s;
+    }
+    public void te(){
+        System.out.println("test");
+    }
+}
